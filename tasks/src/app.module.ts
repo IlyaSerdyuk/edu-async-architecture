4f6ebb2578a7cef60f299b105bd34c5f201e3836
Ilya Serdyuk <ilya@serdyuk.pro>
2022-05-12 18:46:12 +0300

    Подготовил приложение: подключил ORM, Kafka, проверку данных; убрал шаблонный сервис и контроллер

--- tasks/src/app.module.ts
+++ tasks/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'edu_tasks',
+      password: 'edu_tasks',
+      database: 'edu_tasks',
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
